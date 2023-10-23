422b2be13f0f9a8beb3989d59f8b33e1aca6eba6
Enrico Schulz <107748954+SchulzEnrico@users.noreply.github.com>
2023-10-23 12:31:16 +0200

    Test-writing done

--- src/test/java/de/es/kanban/Kanban/TodoControllerTest.java
+++ src/test/java/de/es/kanban/Kanban/TodoControllerTest.java
@@ -1,7 +1,167 @@
 package de.es.kanban.Kanban;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class TodoControllerTest {
+
+    private final static String BASE_URI = "/api/todo";
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    TodoRepo todoRepo;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    @DirtiesContext
+    void getAllTodos() throws Exception {
+        mockMvc.perform(get(BASE_URI))
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
+    }
+
+    @Test
+    @DirtiesContext
+    void getAllTodos_whenEntriesInDb_expectStatus200AndReturnEntriesAsJson() throws Exception {
+        //GIVEN
+        Todo todo = new Todo( "1","test", TodoStatus.OPEN);
+        String todoAsJson = objectMapper.writeValueAsString(todo);
+        //WHEN
+        MvcResult result = mockMvc.perform(post(BASE_URI)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(todoAsJson)
+                )
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Todo savedTodo = objectMapper.readValue(result.getResponse().getContentAsString(), Todo.class);
+
+        List<Todo> todos = List.of(savedTodo);
+        String todosAsJson = objectMapper.writeValueAsString(todos);
+
+        //THEN
+        mockMvc.perform(get(BASE_URI))
+                .andExpect(status().isOk())
+                .andExpect(content().json(todosAsJson));
+    }
+
+    @Test
+    @DirtiesContext
+    void postTodo() throws Exception {
+        //WHEN
+        mockMvc.perform(post("/api/todo")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                        "description": "test-description",
+                                        "status": "OPEN"
+                                    }
+                                """)
+                )
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                "description": "test-description",
+                                "status": "OPEN"
+                            }
+                        """))
+                .andExpect(jsonPath("$.id").isNotEmpty());
+    }
+    @Test
+    @DirtiesContext
+    void putTodo() throws Exception {
+        //GIVEN
+        Todo existingTodo = new Todo("1", "test-description", TodoStatus.OPEN);
+
+        todoRepo.save(existingTodo);
+
+        //WHEN
+        mockMvc.perform(put("/api/todo/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                                    {
+                                        "description": "test-description-2",
+                                        "status": "IN_PROGRESS"
+                                    }
+                                """))
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                "id": "1",
+                                "description": "test-description-2",
+                                "status": "IN_PROGRESS"
+                            }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void getById() throws Exception {
+        //GIVEN
+        Todo existingTodo = new Todo("1", "test-description", TodoStatus.OPEN);
+        todoRepo.save(existingTodo);
+
+        //WHEN
+        mockMvc.perform(get("/api/todo/1"))
+                //THEN
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                "id": "1",
+                                "description": "test-description",
+                                "status": "OPEN"
+                            }
+                        """));
+
+    }
+
+    @Test
+    @DirtiesContext
+    void getByIdTest_whenInvalidId_thenStatus404() throws Exception {
+        mockMvc.perform(get("/api/todo/1"))
+                //THEN
+                .andExpect(status().isNotFound());
+
+    }
+
+
+    @Test
+    @DirtiesContext
+    void deleteTodoById() throws Exception {
+        //GIVEN
+        Todo existingTodo = new Todo("1", "test-description", TodoStatus.OPEN);
+        todoRepo.save(existingTodo);
+
+        //WHEN
+        mockMvc.perform(delete("/api/todo/1"))
+                //THEN
+                .andExpect(status().isOk());
+    }
+
 
 }
